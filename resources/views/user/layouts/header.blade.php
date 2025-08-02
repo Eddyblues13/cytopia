52ec92533735f3eadf8b96f95cd67b40da15ade9
blueswayne133 <blueswayne133@gmail.com>
2025-08-02 11:30:15 -0700

    update

--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -26,7 +26,7 @@
     <!-- Smartsupp Live Chat script -->
     <script type="text/javascript">
         var _smartsupp = _smartsupp || {};
-_smartsupp.key = '6cad4f45ec2dc44e805e6f15b64aff82bb39d26b';
+_smartsupp.key = '6ace84c6f473b0560c197b7390e84a321a04530d';
 window.smartsupp||(function(d) {
   var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
   s=d.getElementsByTagName('script')[0];c=d.createElement('script');
